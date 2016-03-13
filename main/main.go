87f9bfd12615f1f6c5b56a27594b20a6ac145aff
oking02 <thorax123>
2016-03-13 12:37:33 +0000

    Got a basic set staff working. Using hardcoded url, auth, names etc.

--- main/main.go
+++ main/main.go
@@ -2,13 +2,31 @@ package main
 
 import (
 	"fmt"
-	_ "io/ioutil"
-//	"setstaff"
+	"os"
+	"github.com/oking02/PhotoImporter/setstaff"
 )
 
 func main()  {
 
 	fmt.Println("Hello World!")
+
+	args := os.Args[1:]
+
+	argslength := len(args)
+	fmt.Println("Number of arguments: ", argslength)
+
+	switch args[0] {
+
+	case "get":
+		fmt.Println("Get Staff")
+		break
+	case "set":
+		setstaff.SetStaff(args[1:])
+		break
+	default:
+		fmt.Println("Default")
+
+	}
 
 }
 
--- setstaff/setstaff.go
+++ setstaff/setstaff.go
@@ -2,10 +2,64 @@ package setstaff
 
 import (
 	"fmt"
+	"os"
+	"log"
+	"encoding/base64"
+	_"image"
+	_"image/jpeg"
+	_ "io/ioutil"
+	_"strings"
+	"image"
+	"bytes"
+	"image/jpeg"
+	"net/http"
 )
 
 func SetStaff(args []string ) {
 
+	client := &http.Client{}
+
 	fmt.Println("Set Staff")
+
+	imagePath := args[0]
+//	name := args[1]
+//	uniqueId := args[2]
+
+	imgBase64 := getImage(imagePath)
+
+	r, _ := http.NewRequest("POST", "http://localhost:8080/WardMonitor/importTool/set/Bob/Bob[Unique]", bytes.NewBufferString(imgBase64))
+
+	r.SetBasicAuth("ward", "monitor")
+
+	_, err := client.Do(r)
+
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	fmt.Println("Sent")
+
+}
+
+func getImage(path string) string{
+
+	reader, err := os.Open(path)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer reader.Close()
+
+	buffer := new(bytes.Buffer)
+
+
+	m, _, err := image.Decode(reader)
+
+	if err := jpeg.Encode(buffer, m, nil); err != nil {
+		log.Fatal(err)
+	}
+
+	return base64.StdEncoding.EncodeToString(buffer.Bytes())
 
 }
